6eb05de0e88bf5a56cfcbeb27081aab48e7d8aef
3nDr0n <3nDr0n@3NDR0N-PC>
2023-11-11 15:58:05 +0000

    Add project files.

---
+++ MainMethodAssignment/MathMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MainMethodAssignmet
+{
+    public class MathMethods
+    {
+        //First Method that takes an integer
+        public int MathOp(int number)
+        {
+            return number + 7;
+        }
+        //Second method that takes a decimal
+        public int MathOp(decimal number)
+        {
+            //Convert the decimal to an int to perform a math operation
+            int newnum = Convert.ToInt32(number);
+            return newnum * 20;
+        }
+        //Third MathOp method that takes a string
+        public int MathOp(string number)
+        {
+            //Convert the string to an int to perform math operation
+            int newnum = Convert.ToInt32(number);
+            return newnum % 3;
+        }
+    }
+
+}
---
+++ MethodClassAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// Define a class named MathOperations
+public class MathOperations
+{
+    // Define a void method named PerformOperation that takes two integers as parameters
+    public void PerformOperation(int firstNumber, int secondNumber)
+    {
+        // Do a math operation on the first integer (for example, multiply by 2)
+        int result = firstNumber * 2;
+
+        // Display the second integer to the screen
+        Console.WriteLine($"Result of the operation: {result}, Second Number: {secondNumber}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Instantiate the MathOperations class
+        MathOperations mathObj = new MathOperations();
+
+        // Call the PerformOperation method, passing in two numbers (5 and 10)
+        mathObj.PerformOperation(5, 10);
+
+        // Call the PerformOperation method, specifying the parameters by name
+        mathObj.PerformOperation(firstNumber: 8, secondNumber: 15);
+    }
+}
+
